39a6a2f43a377d8511bceed94555dbd80c31f8fc
kondyan <arsenij.kondratenko@gmail.com>
2024-04-14 18:29:33 +0200

    refactoring and shorting portfolio

--- js/index.js
+++ js/index.js
@@ -154,3 +154,137 @@ const refTeam = document.querySelector(".team-list");
 if (refTeam) {
   refTeam.innerHTML = layout.join(" ");
 }
+
+const porfolioProducts = [
+  {
+    id: 1,
+    photo: "./img/picture-1",
+    title: "Banking App Interface Concept",
+    description: "App",
+  },
+  {
+    id: 2,
+    photo: "./img/picture-2",
+    title: "Cashless Payment",
+    description: "Marketing",
+  },
+  {
+    id: 3,
+    photo: "./img/picture-3",
+    title: "Meditation App",
+    description: "App",
+  },
+  {
+    id: 4,
+    photo: "./img/picture-4",
+    title: "Taxi Service",
+    description: "Marketing",
+  },
+  {
+    id: 5,
+    photo: "./img/picture-5",
+    title: "Screen Illustrations",
+    description: "Design",
+  },
+  {
+    id: 6,
+    photo: "./img/picture-6",
+    title: "Online Courses",
+    description: "Marketing",
+  },
+  {
+    id: 7,
+    photo: "./img/picture-7",
+    title: "Instagram Stories Concept",
+    description: "Design",
+  },
+  {
+    id: 8,
+    photo: "./img/picture-8",
+    title: "Organic Food",
+    description: "Web Site",
+  },
+  {
+    id: 9,
+    photo: "./img/picture-9",
+    title: "Fresh Coffee",
+    description: "Web Site",
+  },
+];
+
+const portfolioCard = (card) => {
+  return ` <li class="portfolio-list-item">
+              <a class="portfolio-list-item-link link" href="#">
+                <div class="portfolio-item-wraper">
+                  <picture>
+                    <source
+                      srcset="
+                        ${card.photo}-p.webp    1x,
+                        ${card.photo}-p@2x.webp 2x
+                      "
+                      type="image/webp"
+                      media="(max-width: 767px)"
+                    />
+
+                    <source
+                      srcset="
+                        ${card.photo}-t.webp    1x,
+                        ${card.photo}-t@2x.webp 2x
+                      "
+                      type="image/webp"
+                      media="(max-width: 1439px)"
+                    />
+
+                    <source
+                      srcset="
+                        ${card.photo}.webp    1x,
+                        ${card.photo}@2x.webp 2x
+                      "
+                      type="image/webp"
+                    />
+
+                    <source
+                      srcset="
+                        ${card.photo}-p.jpg    1x,
+                        ${card.photo}-p@2x.jpg 2x
+                      "
+                      media="(max-width: 767px)"
+                    />
+
+                    <source
+                      srcset="
+                        ${card.photo}-t.jpg    1x,
+                        ${card.photo}-t@2x.jpg 2x
+                      "
+                      media="(max-width: 1439px) "
+                    />
+
+                    <source
+                      srcset="${card.photo}.jpg 1x, ${card.photo}@2x.jpg 2x"
+                    />
+
+                    <img
+                      class="portfolio-list-img"
+                      src="${card.photo}.jpg"
+                      alt="${card.title}"
+                    />
+                  </picture>
+                  <div class="portfolio-item-movie">
+                    14 Stylish and User-Friendly App Design Concepts · Task
+                    Manager App · Calorie Tracker App · Exotic Fruit Ecommerce
+                    App · Cloud Storage App
+                  </div>
+                </div>
+                <div class="portfolio-list-item-wraper">
+                  <h3 class="header-three">${card.title}</h3>
+                  <p class="portfolio-list-item-text">${card.description}</p>
+                </div>
+              </a>
+            </li> `;
+};
+
+const portfolioLayout = porfolioProducts.map(portfolioCard);
+const refPorfolio = document.querySelector(".portfolio-list");
+if (refPorfolio) {
+  refPorfolio.innerHTML = portfolioLayout.join(" ");
+}
